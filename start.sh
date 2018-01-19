d28e556690f9dbc42d847a584828195621fd8ae0
gabriel <gabriel.devlog@gmail.com>
2018-01-19 10:54:44 +0100

    Enable CORS in server response

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 
+# Add header module to apache
+a2enmod headers
+
 service apache2 restart
 
 # avoid containers closing and keep it open
