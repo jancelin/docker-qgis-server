79cb135efea6b8d4324df1bcd5bed795a72904d2
ANCELIN Julien <julien.ancelin@inra.fr>
2018-07-12 12:17:08 +0200

    xvfb

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@ set -e
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
+#xvfb
+/usr/bin/Xvfb  :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
 
 service apache2 restart
+
 
 # avoid containers closing and keep it open
 tail -f /dev/null
